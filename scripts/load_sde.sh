09af5152286e44180cc85055ffb6f690b2b16fca
Harry Reeder <harry@harryreeder.co.uk>
2019-07-27 19:43:12 +0100

    🐛 Export PG variables

--- scripts/load_sde.sh
+++ scripts/load_sde.sh
@@ -14,7 +14,7 @@ md5sum -c "$DUMP_FILE.bz2.md5"
 
 bunzip2 "$DUMP_FILE.bz2"
 
-PGUSER=`cat /etc/evept/db/credentials/POSTGRES_USER`
-PGPASSWORD=`cat /etc/evept/db/credentials/POSTGRES_PASSWORD`
+export PGUSER=`cat /etc/evept/db/credentials/POSTGRES_USER`
+export PGPASSWORD=`cat /etc/evept/db/credentials/POSTGRES_PASSWORD`
 
 pg_restore -h evept-db -d evept $DUMP_FILE
