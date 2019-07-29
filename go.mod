c9d37899e2a974a5e67d62f4cff8e1338e44354f
Harry Reeder <harry@harryreeder.co.uk>
2019-07-29 13:35:25 +0100

    🚧 go-redis work

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/codegangsta/negroni v1.0.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-pg/pg/v9 v9.0.0-beta.1
+	github.com/go-redis/redis v6.15.2+incompatible
 	github.com/gorilla/context v1.1.1
 	github.com/gorilla/mux v1.7.3
 	github.com/guregu/dynamo v1.0.0
--- services/rolodex/routes/routes.go
+++ services/rolodex/routes/routes.go
@@ -18,11 +18,13 @@ func Index(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Println("Connecting")
 	client := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"127.0.0.1:6379"},
+		Addrs: []string{"evept-queue:6379"},
 	})
 	fmt.Println("connected")
 	client.Ping()
 	fmt.Println("pung")
+	client.Incr("testkey")
+	fmt.Println("Done")
 
 	web.ReturnJSON(w, &IndexResponse{
 		Message: fmt.Sprintf("Hello, %v", props.Auth0User),
