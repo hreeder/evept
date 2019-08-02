6f431a284151b6eab652ef393f7ab970edaf6e73
Harry Reeder <harry@harryreeder.co.uk>
2019-08-03 00:52:14 +0100

    ✨ Better parse multiple hosts

--- pkg/util/queue/redis.go
+++ pkg/util/queue/redis.go
@@ -1,12 +1,14 @@
 package queue
 
 import (
+	"strings"
+
 	"github.com/go-redis/redis"
 )
 
 // RedisClient returns the redis.ClusterClient object ready to be used
 func (c *Config) RedisClient() redis.UniversalClient {
 	return redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs: []string{c.Host},
+		Addrs: strings.Split(c.Host, ","),
 	})
 }
