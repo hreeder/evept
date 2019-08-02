b2ee66d1de735b8ec2fe675fbce4a2c8b21e8263
Harry Reeder <harry@harryreeder.co.uk>
2019-08-02 23:48:24 +0100

    ✨ Direct input of redis hostname

--- pkg/util/queue/redis.go
+++ pkg/util/queue/redis.go
@@ -1,14 +1,12 @@
 package queue
 
 import (
-	"fmt"
-
 	"github.com/go-redis/redis"
 )
 
 // RedisClient returns the redis.ClusterClient object ready to be used
 func (c *Config) RedisClient() redis.UniversalClient {
 	return redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs: []string{fmt.Sprintf("%v:6379", c.Host)},
+		Addrs: []string{c.Host},
 	})
 }
